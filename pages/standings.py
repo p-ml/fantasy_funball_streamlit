6b2c4b40a9d9c8f789fff20ae98a7d391fd3d9c3
p-ml <34091508+pjjml@users.noreply.github.com>
2021-08-04 22:06:39 +0100

    Add update standings button

--- pages/standings.py
+++ pages/standings.py
@@ -5,12 +5,18 @@ import pandas as pd
 import requests
 import streamlit as st
 
+FANTASY_FUNBALL_URL = os.environ.get("FANTASY_FUNBALL_URL")
+
+
+def _update_standings():
+    """Wrapper to make update_standings request callable"""
+    requests.get(f"{FANTASY_FUNBALL_URL}/update_standings/"),
+
 
 def standings_app():
     st.subheader("Standings")
 
-    fantasy_funball_url = os.environ.get("FANTASY_FUNBALL_URL")
-    funballers = requests.get(f"{fantasy_funball_url}funballer/")
+    funballers = requests.get(f"{FANTASY_FUNBALL_URL}funballer/")
     funballers_text = json.loads(funballers.text)
 
     funballer_names = [x["first_name"] for x in funballers_text]
@@ -29,3 +35,11 @@ def standings_app():
             }
         )
     )
+
+    update_standings_button = st.button(
+        label="Update Standings",
+        on_click=_update_standings(),
+    )
+
+    if update_standings_button:
+        st.markdown("Standings updated! :white_check_mark:")
